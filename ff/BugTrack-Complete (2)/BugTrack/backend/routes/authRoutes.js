4040ac8d68f4bc13fe2e8513a5f9dee7c72b146a
Priyanshu Gupta <priyanshugupta1110@gmail.com>
2026-04-14 00:05:38 +0530

    final chaanges

--- ff/BugTrack-Complete (2)/BugTrack/backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { register, login, profile } = require('../controllers/authController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/register', register);
-router.post('/login',    login);
-router.get('/profile',   protect, profile);
-
-module.exports = router;
