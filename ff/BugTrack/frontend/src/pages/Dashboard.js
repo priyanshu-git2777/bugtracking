aecc13288c8ba23f50746768b8b36d6ae4779407
priyanshu jaggi <priyanshujaggi188@gmail.com>
2026-04-20 01:40:21 +0530

    Update Dashboard.js

--- ff/BugTrack/frontend/src/pages/Dashboard.js
+++ ff/BugTrack/frontend/src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement } from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement } from 'chart.js';   
 import { Doughnut, Bar } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
